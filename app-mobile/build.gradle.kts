6757e384bc6c2346a912aedfc63e208f3766246c
Frederik <typ1.diafreddy@gmail.om>
2026-09-20 10:08:18 +0200

    Fix clean mobile preview resource generation

--- app-mobile/build.gradle.kts
+++ app-mobile/build.gradle.kts
@@ -15,24 +15,24 @@ val syncSugarliciousAnalogPreviewAssets =
         )
         from(
             rootProject.file(
-                "watchfaces/sugarlicious-analog/src/main/res/drawable-nodpi/second_hand.png",
+                "watchfaces/sugarlicious-shared/res/drawable-nodpi/second_hand.xml",
             ),
         ) {
-            rename { "sugarlicious_analog_second_hand.png" }
+            rename { "sugarlicious_analog_second_hand.xml" }
         }
         from(
             rootProject.file(
-                "watchfaces/sugarlicious-analog/src/main/res/drawable-nodpi/hour_hand.png",
+                "watchfaces/sugarlicious-shared/res/drawable-nodpi/hour_hand.xml",
             ),
         ) {
-            rename { "sugarlicious_analog_hour_hand.png" }
+            rename { "sugarlicious_analog_hour_hand.xml" }
         }
         from(
             rootProject.file(
-                "watchfaces/sugarlicious-analog/src/main/res/drawable-nodpi/minute_hand.png",
+                "watchfaces/sugarlicious-shared/res/drawable-nodpi/minute_hand.xml",
             ),
         ) {
-            rename { "sugarlicious_analog_minute_hand.png" }
+            rename { "sugarlicious_analog_minute_hand.xml" }
         }
         into(generatedAnalogPreviewRes)
         eachFile { path = "drawable-nodpi/$name" }
